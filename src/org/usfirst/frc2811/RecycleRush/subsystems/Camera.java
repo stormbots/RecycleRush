bb38c66ba2945778e461c05d72671d0ca2bb944e
Jacob <jacobp98@comcast.net>
2015-02-12 17:57:40 -0800

    documentation!
    commented the code so it's easy to understand

--- src/org/usfirst/frc2811/RecycleRush/subsystems/Camera.java
+++ src/org/usfirst/frc2811/RecycleRush/subsystems/Camera.java
@@ -1,19 +1,18 @@
 package org.usfirst.frc2811.RecycleRush.subsystems;
 
-import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj.command.Subsystem;
 import edu.wpi.first.wpilibj.networktables.NetworkTable;
 import edu.wpi.first.wpilibj.tables.TableKeyNotDefinedException;
 
-public class Camera extends Subsystem {
-	private static double x;
-	private static double y;
-	
+public class Camera extends Subsystem { //This subsystem gets object coordinates from RoboRealm
+	private static double x;			//through a network table that converts them from Visual Basic data
+	private static double y;			//to Java data that we can use. It then returns that data in the
+										//form of the doubles x and y.
 	//static NetworkTable server;
 	static NetworkTable server = NetworkTable.getTable("SmartDashboard");
 	
-	public static void init(){
-		if(server.isConnected() == true){
+	public static void init(){			  //Runs once in AutonomousInit to check
+		if(server.isConnected() == true){ //if the code can retrieve data from the network table.
 			System.out.println("Connection established.");
 		}else{
 			System.out.println("Cannot access the network table!");
@@ -27,9 +26,11 @@ public class Camera extends Subsystem {
 		//put code here
 		System.out.println(server.isConnected());
 		if(server.isConnected() == true){
-			try{
-				x = server.getNumber("COG_X", -3);
-				y = server.getNumber("COG_Y", -2);
+			try{ //RoboRealm draws a bounding box around yellow objects, and records the coordinates
+				 //of the center of gravity of said box in the variables COG_X and COG_Y.
+				 //NetworkTables transfers the Visual Basic data from RoboRealm into Java data we can use.
+				x = server.getNumber("COG_X", -3); //-3 and -2 are used as default values so it will be clear when
+				y = server.getNumber("COG_Y", -2); //COG_X and COG_Y are returning default values.
 			}catch(TableKeyNotDefinedException ex){
 				
 			}
@@ -48,8 +49,8 @@ public class Camera extends Subsystem {
 		// TODO Auto-generated method stub
 		
 	}
-	public static double getX(){
-		update();
+	public static double getX(){		 //These methods return the values retrieved in the 
+		update();                        //update method as doubles.
 		System.out.println("Getting X");
 		return x;
 	}
